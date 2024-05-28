f4193b997fdaf7c75cd3790846bc5469c04b24ac
Nolhan <nolhan.hubert@gmail.com>
2024-05-28 22:43:18 +0200

    changed UUID form

--- src/ble_handler.py
+++ src/ble_handler.py
@@ -81,8 +81,8 @@ class BLEHandler:
             asyncio.create_task(self.update_audio_value(bytearray("Initial audio data", 'utf-8')))
 
     async def update_audio_value(self, data):
-        uuid_bytes = uuid.UUID(self.audio_data_char_uuid).bytes  # Convertir l'UUID en bytes
-        await self.server.update_value(uuid_bytes, data)
+        uuid_str = str(uuid.UUID(self.audio_data_char_uuid))  # Convertir l'UUID en chaîne de caractères
+        await self.server.update_value(uuid_str, data)
         logger.debug(f"Audio data sent: {data[:10]}...")  # Log first 10 bytes for brevity
         logger.debug(f"Audio data packet sent: {data}")
 
