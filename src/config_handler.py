0c766fa619cf996fd80e8f90d58a832815bb3694
Nolhan <nolhan.hubert@gmail.com>
2024-05-28 22:19:25 +0200

    added commentary

--- src/config_handler.py
+++ src/config_handler.py
@@ -4,9 +4,9 @@ class ConfigHandler:
             'device_name': 'Friend',
             'battery_service_uuid': '0000180F-0000-1000-8000-00805F9B34FB',
             'battery_char_uuid': '00002A19-0000-1000-8000-00805F9B34FB',
-            'main_service_uuid': '19B10000-E8F2-537E-4F6C-D104768A1214',
-            'audio_data_char_uuid': '19B10001-E8F2-537E-4F6C-D104768A1214',
-            'codec_type_char_uuid': '19B10002-E8F2-537E-4F6C-D104768A1214'
+            'main_service_uuid': '19B10000-E8F2-537E-4F6C-D104768A1214',  # UUID du service audio
+            'audio_data_char_uuid': '19B10001-E8F2-537E-4F6C-D104768A1214',  # UUID de la caractéristique audio
+            'codec_type_char_uuid': '19B10002-E8F2-537E-4F6C-D104768A1214'  # UUID de la caractéristique du type de codec
         }
 
     def get_config(self):
